123e6071feeff6fa4f3802fe84468e237d842c06
abcpig66 <166998090+abcpig66@users.noreply.github.com>
2024-05-09 00:29:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  2 21:16:35 2021
-
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
-
-Line Bot聊天機器人
-第三章 互動回傳功能
-傳送貼圖StickerSendMessage
-"""
-import requests
-import json
-import time
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -20,60 +7,75 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-import re
+import pandas as pd
+import requests
+import time
+import json
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('980186763ec26279c6c95254f44a4ae8')
 
-# 證交所 API 請求函式
-def get_stock_price(stock_code):
-    url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210507&stockNo={stock_code}"
-    response = requests.get(url)
-    data = response.json()
-    return data
+# 打算要取得的股票代碼
+stock_list_tse = ['0050', '0056', '2330', '2317', '1216']
+stock_list_otc = ['6547', '6180']
+    
+# 組合API需要的股票清單字串
+stock_list1 = '|'.join('tse_{}.tw'.format(stock) for stock in stock_list_tse) 
+stock_list2 = '|'.join('otc_{}.tw'.format(stock) for stock in stock_list_otc) 
+stock_list = stock_list1 + '|' + stock_list2
 
-# 訊息傳遞區塊
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = text=event.message.text
-    if re.match('睡', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('好', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='2'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    # 繼續新增其他貼圖...
-    elif re.match('鴻海價格', message):
-        stock_data = get_stock_price('2317')
-        latest_price = float(stock_data['data'][0][6])  # 最近收盤價
-        if latest_price >= 170:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="鴻海股價已達到或超過170元"))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="鴻海股價未達到170元"))
+#　組合完整的URL
+query_url = f'http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch={stock_list}'
+
+# 呼叫股票資訊API
+def get_stock_info():
+    response = requests.get(query_url)
+    if response.status_code != 200:
+        raise Exception('取得股票資訊失敗.')
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-       
+        data = json.loads(response.text)
+        columns = ['c','n','z','tv','v','o','h','l','y', 'tlong']
+        df = pd.DataFrame(data['msgArray'], columns=columns)
+        df.columns = ['股票代號','公司簡稱','成交價','成交量','累積成交量','開盤價','最高價','最低價','昨收價', '資料更新時間']
+        df.insert(9, "漲跌百分比", 0.0) 
+
+        def count_per(x):
+            if isinstance(x[0], int) == False:
+                x[0] = 0.0
+            result = (x[0] - float(x[1])) / float(x[1]) * 100
+            return pd.Series(['-' if x[0] == 0.0 else x[0], x[1], '-' if result == -100 else result])
+
+        df[['成交價', '昨收價', '漲跌百分比']] = df[['成交價', '昨收價', '漲跌百分比']].apply(count_per, axis=1)
+
+        def time2str(t):
+            t = int(t) / 1000 + 8 * 60 * 60. # UTC時間加8小時為台灣時間
+            return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t))
+
+        df['資料更新時間'] = df['資料更新時間'].apply(time2str)
+        return df
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
+# 處理收到的訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = event.message.text
+    if message == '股票資訊':
+        stock_info_df = get_stock_info()
+        stock_info_text = stock_info_df.to_string(index=False)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=stock_info_text))
+
 # 主程式
 if __name__ == "__main__":
-    # 開啟 Flask 伺服器
     app.run(host='0.0.0.0', port=5000)
