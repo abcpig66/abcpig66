9f3afb6306c00239a56cc498299c5ef5c9cff91b
abcpig66 <166998090+abcpig66@users.noreply.github.com>
2024-05-09 00:18:42 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 22 12:39:41 2019
+Created on Wed Jun  2 21:16:35 2021
 
 @author: Ivan
-"""
-from fugle_realtime import intraday
-import schedule
-import time
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 
-# Line Bot部分
+Line Bot聊天機器人
+第三章 互動回傳功能
+傳送貼圖StickerSendMessage
+"""
+import requests
+import json
+import time
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -24,23 +27,18 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('980186763ec26279c6c95254f44a4ae8')
 
-line_bot_api.push_message('Uae4d95a8996273cbd5fd013544cb3d5a', TextSendMessage(text='你可以開始了'))
+# 證交所 API 請求函式
+def get_stock_price(stock_code):
+    url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210507&stockNo={stock_code}"
+    response = requests.get(url)
+    data = response.json()
+    return data
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
+# 訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.message.text
-    if re.match('睡',message):
+    message = text=event.message.text
+    if re.match('睡', message):
         sticker_message = StickerSendMessage(
             package_id='1',
             sticker_id='1'
@@ -52,95 +50,30 @@ def handle_message(event):
             sticker_id='2'
         )
         line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('驚', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='3'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('請求', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='4'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('美好', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='5'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('生氣', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='6'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('是你', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='7'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('怕', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='8'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('衰', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='9'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('笑', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='10'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('關鍵字',message):
-        flex_message = TextSendMessage(text='以下有雷，請小心',
-                               quick_reply=QuickReply(items=[
-                                   QuickReplyButton(action=MessageAction(label="關鍵價位", text="關鍵！")),
-                                   QuickReplyButton(action=MessageAction(label="密碼", text="密碼！")),
-                                   QuickReplyButton(action=MessageAction(label="到價", text="到價提醒！")),
-                                   QuickReplyButton(action=MessageAction(label="重要筆記", text="重要！！")),
-                                   QuickReplyButton(action=MessageAction(label="早安", text="早安！")),
-                                   QuickReplyButton(action=MessageAction(label="歡迎", text="歡迎！")),
-                                   QuickReplyButton(action=MessageAction(label="貼圖", text="笑！")),                               
-                               ]))
-        line_bot_api.reply_message(event.reply_token, flex_message)
-    elif re.match('到價提醒',message): # 新增的部分
-        stock_info = "目前監控的股票及其價格：\n"
-        for i, j in zip(allstock, allprice):
-            stock_info += f"{i}: {j}\n"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=stock_info))
+    # 繼續新增其他貼圖...
+    elif re.match('鴻海價格', message):
+        stock_data = get_stock_price('2317')
+        latest_price = float(stock_data['data'][0][6])  # 最近收盤價
+        if latest_price >= 170:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="鴻海股價已達到或超過170元"))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="鴻海股價未達到170元"))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+       
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
-def stockPrice_check(stock, check_price):
-    stockdf=intraday.trades(apiToken="p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU=", 
-                output="dataframe", 
-                symbolId=stock)
-    nowprice = stockdf['price'].values[-1]
-    if nowprice > check_price:
-        print(stock + ' 目前價格 ' + str(nowprice))
-        
-def job():
-    allstock = ['2330','2002','2382']
-    allprice = [805, 26 , 280]
-    for i,j in zip(allstock, allprice):
-        stockPrice_check(i, j)
-
-second_5_j = schedule.every(3).seconds.do(job)
-
-while True: 
-    schedule.run_pending()
-    time.sleep(1)
-#主程式
-import os
+# 主程式
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # 開啟 Flask 伺服器
+    app.run(host='0.0.0.0', port=5000)
