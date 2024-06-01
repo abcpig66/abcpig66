57911c2454ec00be1586912d8fa668c09b20be56
abcpig66 <166998090+abcpig66@users.noreply.github.com>
2024-06-01 19:55:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun  2 21:16:35 2021
+創建於 2021年6月2日 21:16:35
 
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+作者：Ivan
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡 ivanyang0606@gmail.com
 
-Line Bot聊天機器人
+Line Bot 聊天機器人
 第三章 互動回傳功能
-傳送貼圖StickerSendMessage
+傳送貼圖 StickerSendMessage
 """
 # 載入所需的套件
 from flask import Flask, request, abort
@@ -17,28 +17,31 @@ from linebot.models import *
 import re
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.date import DateTrigger
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
+# 必須放上自己的 Channel Access Token
 line_bot_api = LineBotApi('p7Cmx4BoCNt0LD2kgdfeOe75gPTHF3sLGrR099KNnnrTdJK5RBzaAxB58kQs7XWmOlKesfndO2M6Nl9q4SeYn7+700i3CqocUHqzN+TeBZoCiktCjDL5w9fLfW9ed++jljaF0zYUhp620TxhWDkeTwdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
+# 必須放上自己的 Channel Secret
 handler = WebhookHandler('980186763ec26279c6c95254f44a4ae8')
 
+# 發送訊息給用戶（測試用）
 line_bot_api.push_message('Uae4d95a8996273cbd5fd013544cb3d5a', TextSendMessage(text='你可以開始了'))
 
-# 監聽所有來自 /callback 的 Post Request
+# 定義回調路徑
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # 獲取 X-Line-Signature 標頭值
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    # 獲取請求主體內容
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # 處理 webhook 主體
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -46,91 +49,28 @@ def callback():
 
     return 'OK'
 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
+# 定義訊息處理函數
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-    if re.match('告訴我秘密', message):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('才不告訴你哩！'))
-    elif re.match('睡', message):
-        # 貼圖查詢：https://developers.line.biz/en/docs/messaging-api/sticker-list/#specify-sticker-in-message-object
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('好', message):
-        # 新增第二個貼圖
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='2'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('驚', message):
-        # 新增第三個貼圖
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='3'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    # 繼續新增其他貼圖...
-    elif re.match('請求', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='4'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('美好', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='5'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('生氣', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='6'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('是你', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='7'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('怕', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='8'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('衰', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='9'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('笑', message):
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='10'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    elif re.match('關鍵字',message):
-        flex_message = TextSendMessage(text='以下有雷，請小心',
-                               quick_reply=QuickReply(items=[
-                                   QuickReplyButton(action=MessageAction(label="關鍵價位", text="關鍵！")),
-                                   QuickReplyButton(action=MessageAction(label="密碼", text="密碼！")),
-                                   QuickReplyButton(action=MessageAction(label="木沐", text="木沐！")),
-                                   QuickReplyButton(action=MessageAction(label="重要筆記", text="重要！！")),
-                                   QuickReplyButton(action=MessageAction(label="早安", text="早安！")),
-                                   QuickReplyButton(action=MessageAction(label="歡迎", text="歡迎！")),
-                                   QuickReplyButton(action=MessageAction(label="貼圖", text="笑！")),                               
-                               ]))
-        line_bot_api.reply_message(event.reply_token, flex_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    response = None
+
+    # 設置日期提醒
+    match = re.match(r'提醒我在 (\d{4}-\d{2}-\d{2} \d{2}:\d{2}) 說 (.+)', message)
+    if match:
+        remind_time = match.group(1)
+        remind_message = match.group(2)
+
+        try:
+            remind_datetime = datetime.strptime(remind_time, '%Y-%m-%d %H:%M')
+            add_reminder(event.source.user_id, remind_datetime, remind_message)
+            response = TextSendMessage(f'好的，我會在 {remind_time} 提醒你：{remind_message}')
+        except ValueError:
+            response = TextSendMessage('請輸入正確的日期時間格式，例如：2024-06-01 14:00')
+
+    if response:
+        line_bot_api.reply_message(event.reply_token, response)
+
 # 定義整點提醒功能
 def hourly_reminder():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -138,12 +78,27 @@ def hourly_reminder():
     # 替換為實際的用戶 ID
     line_bot_api.push_message('Uae4d95a8996273cbd5fd013544cb3d5a', TextSendMessage(text=message))
 
-# 設定定時任務
+# 設置定時任務
 scheduler = BackgroundScheduler()
 scheduler.add_job(hourly_reminder, 'cron', minute=0)
 scheduler.start()
-#主程式
-import os
+
+# 定義增加提醒功能
+def add_reminder(user_id, remind_datetime, message):
+    # 動態創建定時任務
+    scheduler.add_job(
+        func=send_reminder,
+        trigger=DateTrigger(run_date=remind_datetime),
+        args=[user_id, message],
+        id=f"{user_id}_{remind_datetime.strftime('%Y%m%d%H%M%S')}",
+        replace_existing=True
+    )
+
+# 定義發送提醒功能
+def send_reminder(user_id, message):
+    line_bot_api.push_message(user_id, TextSendMessage(text=message))
+
+# 主程式
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
